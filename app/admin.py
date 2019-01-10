6dadb6829e70cce68f2ec4d2911736d8aae2c539
odipojames <odipojames12@gmai.com>
2019-01-11 00:31:47 +0300

    register model classes with admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from app.models import *
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Alert)
+admin.site.register(Comment)
