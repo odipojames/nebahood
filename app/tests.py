02a4f25f002242c3efc5fd05998cf540c6374bd4
odipojames <odipojames12@gmai.com>
2019-01-11 00:29:45 +0300

    unit tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,132 @@
 from django.test import TestCase
-
+from .models import *
+from django.contrib.auth.models import User
+import datetime as dt
+from django.utils import timezone
 # Create your tests here.
+
+class AlertTestClass(TestCase):
+    #Set up Method
+    def setUp(self):
+        '''
+        test case for profiles
+        '''
+        self.user = User(username='odipo')
+        self.user.save()
+        self.hood = Neighborhood(name='Kondele')
+        self.hood.save()
+        self.alert = Alert(alert='New in the hood',posted_by=self.user,neighborhood=self.hood)
+        self.alert.save_alert()
+
+    def tearDown(self):
+        Alert.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.alert,Alert))
+
+    def test_create_business(self):
+        self.alert.save_alert()
+        alert = Alert.objects.all()
+        self.assertTrue(len(alert) > 0)
+
+    def test_delete_neighborhood(self):
+        self.alert.save_alert()
+        self.alert.delete_alert()
+        alert = Alert.objects.all()
+        self.assertTrue(len(alert) == 0)
+
+
+class BusinessTestClass(TestCase):
+    #Set up Method
+    def setUp(self):
+        '''
+        test case for profiles
+        '''
+        self.user = User(username='odipo')
+        self.user.save()
+        self.hood = Neighborhood(name='Kondele')
+        self.hood.save()
+        self.business = Business(name='Fast Foods',propreiter=self.user,neighborhood=self.hood,email='newbusiness@hotmail.com')
+        self.business.create_business()
+
+
+    def tearDown(self):
+        Business.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business,Business))
+
+    def test_create_business(self):
+        self.business.create_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business) > 0)
+
+    def test_delete_neighborhood(self):
+        self.business.create_business()
+        self.business.delete_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business) == 0)
+
+
+
+class ProfileTestClass(TestCase):
+    #Set up Method
+    def setUp(self):
+        '''
+        test case for profiles
+        '''
+        self.hood = Neighborhood(name='Kondele')
+        self.hood.save()
+        self.user = User(username='odipo')
+        self.user.save()
+        self.profile = Profile(photo='black and white',bio='test bio',email="ojamo@xy.com",user=self.user,neighborhood=self.hood)
+        self.profile.save_profile()
+
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) == 0)
+
+
+
+class NeighborhoodTestClass(TestCase):
+    #Set up Method
+    def setUp(self):
+        '''
+        test case for profiles
+        '''
+        self.user = User(username='odipo')
+        self.user.save()
+        self.hood = Neighborhood(name='Kondele',location='Kisumu',description="hood of hoods",photo='photo.url',user=self.user)
+        self.hood.create_neighborhood()
+
+
+    def tearDown(self):
+        Neighborhood.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood,Neighborhood))
+
+    def test_create_neighborhood(self):
+        self.hood.create_neighborhood()
+        hoods = Neighborhood.objects.all()
+        self.assertTrue(len(hoods) > 0)
+
+    def test_delete_neighborhood(self):
+        self.hood.create_neighborhood()
+        self.hood.delete_neighborhood()
+        hood = Neighborhood.objects.all()
+        self.assertTrue(len(hood) == 0)
